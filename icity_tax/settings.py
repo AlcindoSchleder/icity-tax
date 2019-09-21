6c111668cc0558d42f88afd645ad204b15bfe59f
Alcindo Schleder <alcindoschleder@gmail.com>
2019-09-21 20:06:24 -0300

    atualizado o settings.py

--- icity_tax/settings.py
+++ icity_tax/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 from decouple import config
+from dj_database_url import parse as dburl
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -125,3 +126,17 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = 'images'
+
+MEDIA_URL = '/medias/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
